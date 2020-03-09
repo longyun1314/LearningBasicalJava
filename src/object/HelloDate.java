2c27ca8c779d02e09920305b5532a4bd47660858
longyun1314 <2536887754@qq.com>
2020-03-09 22:37:51 +0800

    fix: 公穿创建工程

--- src/object/HelloDate.java
+++ src/object/HelloDate.java
@@ -9,10 +9,6 @@ import java.util.*;
  * @version 4.0
 */
 public class HelloDate {
-  /** Entry point to class & application.
-   * @param args array of string arguments
-   * @throws exceptions No exceptions thrown
-  */
   public static void main(String[] args) {
     System.out.println("Hello, it's: ");
     System.out.println(new Date());
